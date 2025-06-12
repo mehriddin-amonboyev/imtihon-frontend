f4fb7640e9b10859d740c0590fcfae6b3b4ee0ea
mehriddin-amonboyev <mehriddinamonboyev1@gmail.com>
2025-06-12 13:12:54 +0500

    login full

--- src/page/auth/auth.tsx
+++ src/page/auth/auth.tsx
@@ -1,41 +1,43 @@
-// import { useState } from "react";
-// import { useSelector } from "react-redux";
-// import { RootState } from "../../store/store";
-// import { Register } from "./components/register";
-// import { Login } from "./components/login";
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../../store/store";
+import { Register } from "./components/register";
+import { Login } from "./components/login";
 import { LoginForm } from "./components/login-form";
 
 const Auth = () => {
-    // const { error } = useSelector((state: RootState) => state.auth);
-    // const [showLogin, setShowLogin] = useState(true);
+    const { error } = useSelector((state: RootState) => state.auth);
+    const [showLogin, setShowLogin] = useState(true);
 
     return (
-        // <div className="flex flex-col items-center mt-[2.5rem] rounded-[2.5rem] bg-[var(--secondary-color)]">
-        //     <h1 className="pt-10 text-3xl font-medium leading-[130%] text-black pb-[14px]">
-        //         {showLogin ? "Kirish" : "Ro'yxatdan o'tish"}
-        //     </h1>
-        //     <div className="pt-[3px] pl-[42px] pr-[37px] pb-[40px] rounded-[10px] shadow-[0_4px_4px_0_rgba(0,0,0,0.25)]">
-        //         {showLogin ? <Login /> : <Register />}
+        <>
+         {/* <div className="flex flex-col items-center mt-[2.5rem] rounded-[2.5rem] bg-[var(--secondary-color)]">
+             <h1 className="pt-10 text-3xl font-medium leading-[130%] text-black pb-[14px]">
+                 {showLogin ? "Kirish" : "Ro'yxatdan o'tish"}
+             </h1>
+             <div className="pt-[3px] pl-[42px] pr-[37px] pb-[40px] rounded-[10px] shadow-[0_4px_4px_0_rgba(0,0,0,0.25)]">
+                 {showLogin ? <Login /> : <Register />}
 
-        //     </div>
-        //     {error && <p className="text-red-500 text-sm">{error}</p>}
-        //     <div className="flex pt-3">
-        //         <span className="font-secondary font-medium leading-[130%] text-xl text-black">
-        //             {showLogin ? "Ro'yxatdan o'tmaganmisiz?" : "Ro'yxatdan o'tdingizmi?"}
-        //         </span>
-        //         <button
-        //             onClick={() => setShowLogin(!showLogin)}
-        //             className="ml-2 font-secondary font-medium leading-[130%] text-xl text-[#00ced5] cursor-pointer"
-        //         >
-        //             {showLogin ? "Ro'yxatdan o'tish" : "Kirish"}
-        //         </button>
-        //     </div>
-        // </div>
+             </div>
+             {error && <p className="text-red-500 text-sm">{error}</p>}
+             <div className="flex pt-3">
+                 <span className="font-secondary font-medium leading-[130%] text-xl text-black">
+                     {showLogin ? "Ro'yxatdan o'tmaganmisiz?" : "Ro'yxatdan o'tdingizmi?"}
+                 </span>
+                 <button
+                     onClick={() => setShowLogin(!showLogin)}
+                     className="ml-2 font-secondary font-medium leading-[130%] text-xl text-[#00ced5] cursor-pointer"
+                 >
+                     {showLogin ? "Ro'yxatdan o'tish" : "Kirish"}
+                 </button>
+             </div>
+         </div> */}
         <div className="bg-muted flex min-h-full flex-col items-center p-6 md:p-10">
-            <div className="max-w-sm md:max-w-3xl">
+            <div className="max-w-sm md:max-w-md">
                 <LoginForm />
             </div>
         </div>
+        </>
     )
 }
 
--- src/page/auth/components/login-form.tsx
+++ src/page/auth/components/login-form.tsx
@@ -2,53 +2,117 @@ import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import { Card, CardContent } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
 import { SiteLogo } from "@/assets/svg/siteLogo"
+import { useNavigate } from "react-router-dom"
+import { useDispatch } from "react-redux"
+import { useLogin } from "../services/mutation/loginUser"
+import { AppDispatch } from "@/store/store"
+import { setToken, setUser } from "@/store/redux/authSlice"
+import { useForm } from "react-hook-form"
+import { useState } from "react"
+import { Eye, EyeOff } from "lucide-react"
 
+type LoginFormValues = {
+  userName: string;
+  password: string;
+};
+
+type LoginResponse = {
+  accessToken: string;
+  refreshToken: string;
+  user: {
+    id: string;
+    role: string;
+    userName: string;
+  };
+}
 export function LoginForm({
   className,
   ...props
 }: React.ComponentProps<"div">) {
+  const [showPassword, setShowPassword] = useState(false)
+  const navigate = useNavigate();
+  const dispatch = useDispatch<AppDispatch>();
+  const loginMutation = useLogin();
+
+  const { register, handleSubmit, formState: { errors }, } = useForm<LoginFormValues>();
+
+  const handleLogin = (values: LoginFormValues) => {
+    loginMutation.mutate(values, {
+      onSuccess: (data: LoginResponse) => {
+        dispatch(setToken({
+          accessToken: data?.accessToken,
+          refreshToken: data?.refreshToken
+        }))
+        dispatch(setUser({ userId: data?.user.id }));
+        navigate("/app/subjects");
+      },
+      onError: (error) => {
+        console.error("Login failed:", error);
+      },
+    });
+  };
+
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card className="overflow-hidden p-0">
         <CardContent className="grid p-0 md:grid-cols-1">
-          <form className="p-6 md:p-8">
+          <form
+            onSubmit={handleSubmit(handleLogin)}
+            method="POST"
+            className="p-6 md:p-8"
+          >
             <div className="flex flex-col gap-6">
               <div className="flex flex-col items-center text-center">
-                <SiteLogo/>
+                <SiteLogo />
               </div>
-              <div className="grid gap-3">
-                <Label htmlFor="email">Email</Label>
+              <div className="grid gap-6">
                 <Input
-                  id="email"
-                  type="email"
-                  placeholder="m@example.com"
-                  required
+                  id="userName"
+                  type='text'
+                  placeholder="Username"
+                  {...register("userName", { required: "Username kiriting" })}
                 />
-              </div>
-              <div className="grid gap-3">
-                <div className="flex items-center">
-                  <Label htmlFor="password">Password</Label>
-                  <a
-                    href="#"
-                    className="ml-auto text-sm underline-offset-2 hover:underline"
+                {errors.userName && (
+                  <p className="text-red-500 text-sm">{errors.userName.message}</p>
+                )}
+                <div className="relative">
+                  <Input
+                    id="password"
+                    type={showPassword ? 'text' : "password"}
+                    placeholder="Password"
+                    {...register("password", { required: "Parol kiriting" })}
+                  />
+                  <Button
+                    type="button"
+                    variant={"secondary"}
+                    className="absolute right-2 top-1/2 -translate-y-1/2 text-gray-500 bg-muted"
+                    onClick={() => setShowPassword(!showPassword)}
                   >
-                    Forgot your password?
-                  </a>
+                    {showPassword ? <EyeOff size={18} /> : <Eye size={18} />}
+                  </Button>
                 </div>
-                <Input id="password" type="password" required />
               </div>
               <Button type="submit" className="w-full">
-                Login
+                <span className="font-medium text-[1.3rem] text-[var(--secondary-color)]">
+                  Login
+                </span>
               </Button>
+              <div className="flex text-sm justify-between">
+                <a href="#" className="text-sm underline-offset-2 hover:underline">
+                  Parolni unutdingizmi?
+                </a>
+                <a href="#" className="underline-offset-4 text-[var(--primary-blue)]">
+                  Ro'yxatdan o'tish
+                </a>
+              </div>
               <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
                 <span className="bg-card text-muted-foreground relative z-10 px-2">
-                  Or continue with
+                  Google orqali kirish
                 </span>
               </div>
-              <div className="grid grid-cols-3 gap-4">
-                <Button variant="outline" type="button" className="w-full">
+              <div className="grid grid-cols-1 ml-auto mr-auto">
+                {/* <Button variant="outline" type="button" className="w-full">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                     <path
                       d="M12.152 6.896c-.948 0-2.415-1.078-3.96-1.04-2.04.027-3.91 1.183-4.961 3.014-2.117 3.675-.546 9.103 1.519 12.09 1.013 1.454 2.208 3.09 3.792 3.039 1.52-.065 2.09-.987 3.935-.987 1.831 0 2.35.987 3.96.948 1.637-.026 2.676-1.48 3.676-2.948 1.156-1.688 1.636-3.325 1.662-3.415-.039-.013-3.182-1.221-3.22-4.857-.026-3.04 2.48-4.494 2.597-4.559-1.429-2.09-3.623-2.324-4.39-2.376-2-.156-3.675 1.09-4.61 1.09zM15.53 3.83c.843-1.012 1.4-2.427 1.245-3.83-1.207.052-2.662.805-3.532 1.818-.78.896-1.454 2.338-1.273 3.714 1.338.104 2.715-.688 3.559-1.701"
@@ -56,7 +120,7 @@ export function LoginForm({
                     />
                   </svg>
                   <span className="sr-only">Login with Apple</span>
-                </Button>
+                </Button> */}
                 <Button variant="outline" type="button" className="w-full">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                     <path
@@ -66,7 +130,7 @@ export function LoginForm({
                   </svg>
                   <span className="sr-only">Login with Google</span>
                 </Button>
-                <Button variant="outline" type="button" className="w-full">
+                {/* <Button variant="outline" type="button" className="w-full">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                     <path
                       d="M6.915 4.03c-1.968 0-3.683 1.28-4.871 3.113C.704 9.208 0 11.883 0 14.449c0 .706.07 1.369.21 1.973a6.624 6.624 0 0 0 .265.86 5.297 5.297 0 0 0 .371.761c.696 1.159 1.818 1.927 3.593 1.927 1.497 0 2.633-.671 3.965-2.444.76-1.012 1.144-1.626 2.663-4.32l.756-1.339.186-.325c.061.1.121.196.183.3l2.152 3.595c.724 1.21 1.665 2.556 2.47 3.314 1.046.987 1.992 1.22 3.06 1.22 1.075 0 1.876-.355 2.455-.843a3.743 3.743 0 0 0 .81-.973c.542-.939.861-2.127.861-3.745 0-2.72-.681-5.357-2.084-7.45-1.282-1.912-2.957-2.93-4.716-2.93-1.047 0-2.088.467-3.053 1.308-.652.57-1.257 1.29-1.82 2.05-.69-.875-1.335-1.547-1.958-2.056-1.182-.966-2.315-1.303-3.454-1.303zm10.16 2.053c1.147 0 2.188.758 2.992 1.999 1.132 1.748 1.647 4.195 1.647 6.4 0 1.548-.368 2.9-1.839 2.9-.58 0-1.027-.23-1.664-1.004-.496-.601-1.343-1.878-2.832-4.358l-.617-1.028a44.908 44.908 0 0 0-1.255-1.98c.07-.109.141-.224.211-.327 1.12-1.667 2.118-2.602 3.358-2.602zm-10.201.553c1.265 0 2.058.791 2.675 1.446.307.327.737.871 1.234 1.579l-1.02 1.566c-.757 1.163-1.882 3.017-2.837 4.338-1.191 1.649-1.81 1.817-2.486 1.817-.524 0-1.038-.237-1.383-.794-.263-.426-.464-1.13-.464-2.046 0-2.221.63-4.535 1.66-6.088.454-.687.964-1.226 1.533-1.533a2.264 2.264 0 0 1 1.088-.285z"
@@ -74,13 +138,11 @@ export function LoginForm({
                     />
                   </svg>
                   <span className="sr-only">Login with Meta</span>
-                </Button>
+                </Button> */}
               </div>
-              <div className="text-center text-sm">
-                Don&apos;t have an account?{" "}
-                <a href="#" className="underline underline-offset-4">
-                  Sign up
-                </a>
+              <div className="px-10 font-medium text-[1rem] text-muted-foreground *:[a]:hover:text-primary text-center text-balance *:[a]:underline *:[a]:underline-offset-4">
+                By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
+                and <a href="#">Privacy Policy</a>.
               </div>
             </div>
           </form>
@@ -93,10 +155,6 @@ export function LoginForm({
           </div> */}
         </CardContent>
       </Card>
-      <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
-        By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
-        and <a href="#">Privacy Policy</a>.
-      </div>
     </div>
   )
 }
